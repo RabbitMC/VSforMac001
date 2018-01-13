fc496ac660dad554aab9b51d977cdf0514287e5c
Miralem <miralem.cebic@matrix42.com>
2018-01-13 17:09:53 +0100

    first steps

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # VSforMac001
 Simple Visual Studio for Mac Project. Building a complete .NET Core solution on macOS using Visual Studio for Mac
+
+Erstellen einer vollständigen .NET Core-Lösung unter macOS mit Visual Studio für Mac
+from [here](https://docs.microsoft.com/de-de/dotnet/core/tutorials/using-on-mac-vs-full-solution)
---
+++ WordCounter/TextUtils/WordCount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace TextUtils
+{
+    public static class WordCount
+    {
+        public static int GetWordCount(string searchWord, string inputString) 
+        {
+            // Null check these variables and dertermine if they have values
+            if (String.IsNullOrEmpty(searchWord) || String.IsNullOrEmpty(inputString)) 
+            {
+                return 0;
+            }
+
+            // Convert the string into an array of words
+            var source = inputString.Split(new char[] { '.', '?', '!', ' ', ';', ':', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Create the query. Use ToLowerInvariant to match uppercase/lowercase strings.   
+            var matchQuery = from word in source
+                             where word.ToLowerInvariant() == searchWord.ToLowerInvariant()
+                             select word;
+
+            // Count the matches, which executes the query. Return the reuslt.
+            return matchQuery.Count();
+        }
+    }
+}
